ed6bb2535be3e2502309ff490d3851dffd25f017
dinhxuanloc97 <dinhxuanloc741997@gmail.com>
2022-10-13 07:54:08 +0700

    leb

---
+++ src/Bai3/Main.java
@@ -0,0 +1,33 @@
+package Bai3;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+
+        System.out.println("Nhap chuoi ");
+        String string = sc.nextLine();
+
+        //Tim chuoi lien tiep co do dai lon nhat
+        for (int i = 0; i < string.length(); i++) {
+            if (list.size()>1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
+
+                list.clear();//1 cau lenh
+            }
+            list.add(string.charAt(i));
+
+            if (list.size() > max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+        }
+        for(Character ch : max){
+            System.out.println(ch);
+        }
+        System.out.println();
+    }
+}
